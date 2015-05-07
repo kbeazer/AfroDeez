cf0bb7f6c4aa88644364d9ceb82b1f30c3571158
kbeazer <kbeazer@fullsail.edu>
2015-05-06 22:24:45 -0400

    main.js updated

--- js/main.js
+++ js/main.js
@@ -13,4 +13,41 @@ $(document).ready(function(){
 	$('.navHide').click(function(){
 		$('.navHide').slideToggle('slow');	
 	});
+
+	$('.hover').hover(function(){
+		$(this).css('backgroundColor', '#cdcdcd');
+		$('.link').css('color', '#123');
+	},function(){
+		$(this).css('backgroundColor', '#123');
+		$('.link').css('color', '#efefef');
+	});
+
+	$('.hover2').hover(function(){
+		$(this).css('backgroundColor', '#cdcdcd');
+		$('.link2').css('color', '#123');
+	},function(){
+		$(this).css('backgroundColor', '#123');
+		$('.link2').css('color', '#efefef');
+	});
+	$('.hover3').hover(function(){
+		$(this).css('backgroundColor', '#cdcdcd');
+		$('.link3').css('color', '#123');
+	},function(){
+		$(this).css('backgroundColor', '#123');
+		$('.link3').css('color', '#efefef');
+	});
+	$('.hover4').hover(function(){
+		$(this).css('backgroundColor', '#cdcdcd');
+		$('.link4').css('color', '#123');
+	},function(){
+		$(this).css('backgroundColor', '#123');
+		$('.link4').css('color', '#efefef');
+	});
+	$('.hover5').hover(function(){
+		$(this).css('backgroundColor', '#cdcdcd');
+		$('.link5').css('color', '#123');
+	},function(){
+		$(this).css('backgroundColor', '#123');
+		$('.link5').css('color', '#efefef');
+	});
 });
